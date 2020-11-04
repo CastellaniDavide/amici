110b3480fa9880bf4084408bb3b62a8422826d9e
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-04 07:47:43 +0100

    Created bin/template.cpp

---
+++ bin/amici.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file amici.cpp
+ *
+ * @version 01.01 2020-11-4
+ *
+ * @brief https://training.olinfo.it/#/task/amici/statement
+ *
+ * @ingroup amici
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
